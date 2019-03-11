a7096ee260b500526050b79426b555c4bad6a5bb
alishermuhamed <alisher.muhamed@yandex.ru>
2019-03-11 13:31:57 +0300

    Position stats, add skills and bio to profile screen

--- src/screens/app/profile.js
+++ src/screens/app/profile.js
@@ -33,16 +33,22 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#53535F',
   },
-  stat: {
-    marginTop: 15,
-  },
   statRow: {
     flexDirection: 'row',
+    width: '100%',
+    marginTop: 15,
   },
   statElement: {
+    flex: 1,
+    borderColor: '#979797',
     justifyContent: 'center',
     alignItems: 'center',
-    marginHorizontal: 30,
+  },
+  statElementLeft: {
+    borderRightWidth: 0.5,
+  },
+  statElementRight: {
+    borderLeftWidth: 0.5,
   },
   statNumber: {
     fontWeight: 'bold',
@@ -51,11 +57,46 @@ const styles = StyleSheet.create({
   },
   statSign: {
     fontWeight: 'bold',
+    fontSize: 15,
     color: '#53535F',
   },
-  statSeparator: {
-    borderWidth: 0.5,
-    borderColor: '#979797',
+  skills: {
+    flex: 1,
+    width: '100%',
+    marginTop: 30,
+    alignItems: 'center',
+  },
+  skill: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '90%',
+    margin: 5,
+    borderRadius: 30,
+    backgroundColor: '#979797',
+  },
+  skillTag: {
+    fontSize: 15,
+    color: '#FFFFFF',
+    marginLeft: 20,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  skillVerified: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: '#FFFFFF',
+    marginRight: 20,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  bio: {
+    margin: 15,
+    alignItems: 'center',
+  },
+  whois: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: '#53535F',
   },
 });
 
@@ -69,6 +110,8 @@ export default class Profile extends React.Component {
       xp: 0,
       coins: 0,
     },
+    skills: [],
+    bio: '',
   };
 
   componentDidMount = () => {
@@ -82,6 +125,24 @@ export default class Profile extends React.Component {
           xp: 300,
           coins: 617,
         },
+        skills: [
+          {
+            id: 1,
+            tag: 'Backend Developer',
+            verified: 6,
+          },
+          {
+            id: 2,
+            tag: 'Frontend Developer',
+            verified: 4,
+          },
+          {
+            id: 3,
+            tag: 'Product Manager',
+            verified: 9,
+          },
+        ],
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       });
     } catch {
       alert('Error while fetching user data!');
@@ -89,7 +150,13 @@ export default class Profile extends React.Component {
   };
 
   render() {
-    const { pic, username, stat } = this.state;
+    const {
+      pic,
+      username,
+      stat,
+      skills,
+      bio,
+    } = this.state;
     return (
       <View style={styles.view}>
         <Toolbar title="Profile" />
@@ -102,29 +169,41 @@ export default class Profile extends React.Component {
             </View>
           )}
           <Text style={styles.username}>{username}</Text>
-          <View style={styles.stat}>
+          <View>
             <View style={styles.statRow}>
-              <View style={styles.statElement}>
+              <View style={[styles.statElement, styles.statElementLeft]}>
                 <Text style={styles.statNumber}>{stat.hackTotal}</Text>
                 <Text style={styles.statSign}>Total hackathons</Text>
               </View>
-              <View style={styles.statSeparator} />
-              <View style={styles.statElement}>
+              <View style={[styles.statElement, styles.statElementRight]}>
                 <Text style={styles.statNumber}>{stat.hackWin}</Text>
                 <Text style={styles.statSign}>Hackathon wins</Text>
               </View>
             </View>
             <View style={styles.statRow}>
-              <View style={styles.statElement}>
+              <View style={[styles.statElement, styles.statElementLeft]}>
                 <Text style={styles.statNumber}>{stat.xp}</Text>
                 <Text style={styles.statSign}>XP points</Text>
               </View>
-              <View style={styles.statSeparator} />
-              <View style={styles.statElement}>
+              <View style={[styles.statElement, styles.statElementRight]}>
                 <Text style={styles.statNumber}>{stat.coins}</Text>
                 <Text style={styles.statSign}>Total tokens</Text>
               </View>
             </View>
+          </View>
+          <View style={styles.skills}>
+            {
+              skills.map(skill => (
+                <View style={styles.skill} key={skill.id}>
+                  <Text style={styles.skillTag}>{skill.tag}</Text>
+                  <Text style={styles.skillVerified}>{skill.verified}</Text>
+                </View>
+              ))
+            }
+          </View>
+          <View style={styles.bio}>
+            <Text style={styles.whois}>#Whois</Text>
+            <Text>{bio}</Text>
           </View>
         </ScrollView>
       </View>
