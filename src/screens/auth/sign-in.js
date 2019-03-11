3ecd48f9546c213d23d639204f470f11bbf23507
alishermuhamed <alisher.muhamed@yandex.ru>
2019-03-11 16:36:42 +0300

    Add fields and buttons to sign in screen

--- src/screens/auth/sign-in.js
+++ src/screens/auth/sign-in.js
@@ -1,6 +1,58 @@
 import React from 'react';
 import { shape, func } from 'prop-types';
-import { View, Text, Button } from 'react-native';
+import {
+  StyleSheet,
+  KeyboardAvoidingView,
+  TouchableNativeFeedback,
+  View,
+  Text,
+  TextInput,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  telegramButton: {
+    marginTop: 10,
+    backgroundColor: '#3F87F5',
+    borderRadius: 30,
+    alignItems: 'center',
+    width: '50%',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    marginVertical: 10,
+  },
+  credentialsHint: {
+    fontWeight: 'bold',
+    color: '#53535F',
+    marginTop: 10,
+  },
+  input: {
+    marginTop: 10,
+    padding: 0,
+    borderColor: '#979797',
+    borderBottomWidth: 1,
+    width: '80%',
+  },
+  forgotPassword: {
+    marginTop: 20,
+    color: '#979797',
+  },
+  loginButton: {
+    marginTop: 20,
+    backgroundColor: '#3F87F5',
+    width: '80%',
+    alignItems: 'center',
+  },
+  signUp: {
+    marginTop: 20,
+  },
+});
 
 export default class SignIn extends React.Component {
   static propTypes = {
@@ -9,15 +61,55 @@ export default class SignIn extends React.Component {
     }).isRequired,
   };
 
+  state = {
+    email: '',
+    pwd: '',
+  };
+
+  loginWithTelegram = () => {
+    this.props.navigation.navigate('App');
+  };
+
+  login = () => {
+    this.props.navigation.navigate('App');
+  };
+
   render() {
     const { navigation } = this.props;
+    const { email, pwd } = this.state;
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Sign In Screen</Text>
-        <Button title="Login" onPress={() => navigation.navigate('App')} />
-        <Button title="Forgot Password" onPress={() => navigation.navigate('ForgotPassword')} />
-        <Button title="Sign Up" onPress={() => navigation.navigate('SignUp')} />
-      </View>
+      <KeyboardAvoidingView style={styles.view}>
+        <Icon name="user" size={100} color="#3F87F5" />
+        <TouchableNativeFeedback onPress={this.loginWithTelegram}>
+          <View style={styles.telegramButton}>
+            <Text style={styles.buttonText}>Log in with Telegram</Text>
+          </View>
+        </TouchableNativeFeedback>
+        <Text style={styles.credentialsHint}>Or enter your login credentials to get access:</Text>
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={text => this.setState({ email: text })}
+          placeholder="Email address"
+          keyboardType="email-address"
+        />
+        <TextInput
+          style={styles.input}
+          value={pwd}
+          onChangeText={text => this.setState({ pwd: text })}
+          placeholder="Password"
+          secureTextEntry
+        />
+        <TouchableNativeFeedback onPress={() => navigation.navigate('ForgotPassword')}>
+          <Text style={styles.forgotPassword}>Forgot password?</Text>
+        </TouchableNativeFeedback>
+        <TouchableNativeFeedback onPress={this.login}>
+          <View style={styles.loginButton}>
+            <Text style={styles.buttonText}>Log in</Text>
+          </View>
+        </TouchableNativeFeedback>
+        <Text style={styles.signUp} onPress={() => navigation.navigate('SignUp')}>Do not have an account? Sign up</Text>
+      </KeyboardAvoidingView>
     );
   }
 }
