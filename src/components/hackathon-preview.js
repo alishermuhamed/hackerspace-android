a26508414feee65d3522880cd12d6ff526ba5099
alishermuhamed <alisher.muhamed@yandex.ru>
2019-03-11 15:20:07 +0300

    Add hackathon-preview component, fill hackathons screen

---
+++ src/components/hackathon-preview.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableNativeFeedback,
+} from 'react-native';
+import { string } from 'prop-types';
+
+const styles = StyleSheet.create({
+  view: {
+    height: 200,
+    backgroundColor: '#979797',
+    borderBottomWidth: 1,
+    borderColor: '#000000',
+    padding: 15,
+  },
+  text: {
+    color: '#FFFFFF',
+    textShadowColor: '#000000',
+    textShadowOffset: { height: 3 },
+    textShadowRadius: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#3F87F5',
+    marginTop: 70,
+    width: '30%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: '#FFFFFF',
+    margin: 5,
+  },
+});
+
+export default class HackathonPreview extends React.Component {
+  static propTypes = {
+    title: string.isRequired,
+    city: string.isRequired,
+    dateStart: string.isRequired,
+  };
+
+  render() {
+    const {
+      title,
+      city,
+      dateStart,
+    } = this.props;
+    const date = new Date(...dateStart.substring(0, 10).split('-'));
+    return (
+      <View style={styles.view}>
+        <Text style={[styles.text, styles.title]}>{title}</Text>
+        <Text style={styles.text}>{city}</Text>
+        <Text style={styles.text}>{date.toDateString()}</Text>
+        <TouchableNativeFeedback
+          onPress={() => alert('Applied to hackathon!')}
+        >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Apply</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    );
+  }
+}
--- src/screens/app/hackathons.js
+++ src/screens/app/hackathons.js
@@ -1,19 +1,95 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Picker,
+  ScrollView,
+} from 'react-native';
 import Toolbar from '../../components/toolbar';
+import HackathonPreview from '../../components/hackathon-preview';
 
 const styles = StyleSheet.create({
   view: {
     flex: 1,
     alignItems: 'stretch',
   },
+  status: {
+    backgroundColor: '#53535F',
+    color: '#FFFFFF',
+  },
+  hackathons: {
+    alignItems: 'stretch',
+    backgroundColor: '#FFFFFF',
+  },
 });
 
 export default class Hackathons extends React.Component {
+  state = {
+    status: 'open',
+    events: [],
+  };
+
+  changeStatus = status => this.setState({ status });
+
+  componentDidMount = () => {
+    try {
+      this.setState({
+        events: [
+          {
+            id: 1,
+            title: 'Hack.Moscow',
+            city: 'Moscow',
+            dateStart: '2019-04-20T15:00:00.000Z',
+          },
+          {
+            id: 2,
+            title: 'Local Hack Day',
+            city: 'Moscow',
+            dateStart: '2018-07-13T15:00:00.000Z',
+          },
+          {
+            id: 3,
+            title: 'HackUps',
+            city: 'Barcelona',
+            dateStart: '2018-02-13T15:00:00.000Z',
+          },
+          {
+            id: 4,
+            title: 'Hackathon 1',
+            city: 'New York',
+            dateStart: '2018-01-24T15:00:00.000Z',
+          },
+        ],
+      });
+    } catch {
+      alert('Error while loading events!');
+    }
+  };
+
   render() {
+    const { status, events } = this.state;
     return (
       <View style={styles.view}>
         <Toolbar title="Hackathons" />
+        <Picker
+          selectedValue={status}
+          onValueChange={this.changeStatus}
+          style={styles.status}
+        >
+          <Picker.Item label="Open" value="open" />
+          <Picker.Item label="Applied" value="applied" />
+          <Picker.Item label="Won" value="won" />
+          <Picker.Item label="Participated" value="participated" />
+          <Picker.Item label="Confirmed" value="confirmed" />
+          <Picker.Item label="Activated" value="activated" />
+        </Picker>
+        <ScrollView contentContainerStyle={styles.hackathons}>
+          {
+            events.map(event => (
+              <HackathonPreview key={event.id} {...event} />
+            ))
+          }
+        </ScrollView>
       </View>
     );
   }
